c9d6a2f667f1a70392bd3bfbc206f6db6d0d1096
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2025-04-05 00:12:36 +0300

    lib.rs and token.rs

---
+++ src/token.rs
@@ -0,0 +1,47 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    // Keywords
+    Let,
+    Fun,
+    Return,
+    If,
+    Else,
+    While,
+    Print,
+    True,
+    False,
+    IntType,
+    BoolType,
+    StringType,
+    UnitType,
+
+    // Identifiers and literals
+    Identifier(String),
+    Integer(i64),
+    StringLiteral(String),
+
+    // Operators
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Equal,
+    EqualEqual,
+    NotEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Colon,
+
+    // Delimiters
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Comma,
+    Semicolon,
+
+    // End of input
+    EOF,
+}
